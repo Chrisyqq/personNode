f9f9609d7fdef8040860c344258ea79a4e979ad2
Chrisyqq <chrisyqq1993@gmail.com>
2017-05-19 01:50:55 +0800

    no message

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const User = require('./db').User;
  * 监听3300端口
  * 用JSON格式处理bodyParser请求
  */
-app.set('port', (process.env.port || 3300));
+app.set('port', (process.env.port || 3000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 //app.set('view engine', 'ejs');
